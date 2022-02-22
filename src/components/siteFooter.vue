13783fa8daa6bcc67e2ac87af2d327fb49d16c17
Atila Guilherme <atilacarrao@hotmail.com>
2022-02-22 11:53:46 -0300

    links add on footer

--- src/components/siteFooter.vue
+++ src/components/siteFooter.vue
@@ -3,8 +3,8 @@
     <footer class="flexWrap">
       <ul class="flexWrap">
         <li>&copy; Gui 2022</li>
-        <li>GitHub</li>
-        <li>LinkedIn</li>
+        <li><a href="https://github.com/guic97">GitHub</a></li>
+        <li><a href="https://www.linkedin.com/in/atila-guilherme-aa5638219/">LinkedIn</a></li>
       </ul>
     </footer>
   </main>
@@ -27,5 +27,12 @@ export default {
   footer > ul >li{
     list-style-type: none;
     margin: 0 10px;
+  }
+  a{
+    text-decoration: none;
+    color: var(--color1);
+  }
+  a:hover{
+    color: var(--color3);
   }
 </style>
